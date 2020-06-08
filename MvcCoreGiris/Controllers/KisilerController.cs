911f9c270ad159337eb2644d9be2fee7b8afdd5c
hakan ölçer <ha_-kan-_olcer95@windowslive.com>
2020-06-08 19:19:12 +0300

    Add project files.

---
+++ MvcCoreGiris/Controllers/KisilerController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreGiris.Models;
+
+namespace MvcCoreGiris.Controllers
+{
+    public class KisilerController : Controller
+    {
+        private readonly OkulContext db;
+        public KisilerController(OkulContext okulContext)
+        {
+            db = okulContext;
+        }
+
+        // GET: Kisiler
+        public IActionResult Index()
+        {
+            return View(db.Kisiler.ToList());
+        }
+
+        public IActionResult Yeni()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Yeni(Kisi kisi)
+        {
+            if (ModelState.IsValid)
+            {
+                // db.Kisiler.Add(kisi);
+                db.Add(kisi);
+                db.SaveChanges();
+                // Index in adı üstte değişince burda da değişiyor.
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        public IActionResult Duzenle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var kisi = db.Kisiler.Find(id);
+
+            if (kisi == null)
+            {
+                return NotFound();
+            }
+            return View(kisi);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Duzenle(Kisi kisi)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                //db.Entry(kisi).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.Update(kisi);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Sil(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var kisi = db.Kisiler.Find(id);
+
+            if (kisi == null)
+            {
+                return NotFound();
+            }
+            db.Remove(kisi);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
---
+++ MvcCoreGiris/Models/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreGiris.Models
+{
+    public class Kisi
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Ad alanı zorunludur.")]
+        [Display(Name ="Ad")]
+        public string KisiAd { get; set; }
+    }
+}
